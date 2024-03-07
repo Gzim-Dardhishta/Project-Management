7d27e36992776228f726aae82d698ce2ebad1fae
Ximi the Programmer <gzimdardhishta0@gmail.com>
2024-03-07 21:07:26 +0200

    Formated code with Prettierr and EsLint

--- PM-client/src/App.tsx
+++ PM-client/src/App.tsx
@@ -1,12 +1,11 @@
 
 
-function App() {
-
-  return (
-    <>
-      <h1 className='text-7xl text-center font-medium text-[#039e9e]'>Project Management</h1>
-    </>
-  )
+const App = () => {
+    return (
+        <>
+            <h1 className='text-7xl text-center font-medium text-[#039e9e]'>Project Management</h1>
+        </>
+    )
 }
 
 export default App
